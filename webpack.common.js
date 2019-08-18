6fe4b7ddf19b53df2d527f07b25da6ea7e1c51a4
Glazinski <vercety.glaz@gmail.com>
2019-08-18 18:48:07 +0200

    Project initialization
    Add src with template empty scss an js file
    Add webpack and postcss config
    Webpack config contains:
    -Production and development config
    -Babel 7 with transform-runtime plugin which allows to use async await
    -Bundling with hashContent in name
    -Sass compiler
    -Autoprefixer for scss files
    -Minify html size css and js files

---
+++ webpack.common.js
@@ -0,0 +1,18 @@
+module.exports = {
+  entry: './src/app.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
+  }
+};
---
+++ webpack.dev.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const common = require('./webpack.common');
+const merge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = merge(common, {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    filename: 'app-bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/template.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          'postcss-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  }
+});
