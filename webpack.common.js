5ce2903284bcb2c24d4e123bd2e251073e727f39
Michał Kamiński <michal.kaminski218@gmail.com>
2019-12-19 14:08:15 +0100

    Add gh-pages
    Set script in package.json
    Set homepage name in package.json

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,18 @@
 module.exports = {
-   entry: './src/js/app.js',
-   module: {
-      rules: [
-         {
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-               loader: 'babel-loader',
-               options: {
-                  presets: ['@babel/preset-env'],
-                  plugins: ['@babel/plugin-transform-runtime']
-               }
-            }
-         }
-      ]
-   }
+  entry: './src/js/app.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
 };
